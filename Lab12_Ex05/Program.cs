e6f7502fd450e5e8e4a0d9ea78a9b986a68a53c1
nongbluelnwza007 <booblueboyy@gmail.com>
2025-03-25 22:01:22 +0700

    add

---
+++ Lab12_Ex05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        DerivedClass dc = new DerivedClass();
+    }
+}
+
+class BaseClass1
+{
+    public BaseClass1()
+    {
+        System.Console.WriteLine("This is BaseClass1");
+    }
+}
+
+class BaseClass2
+{
+    public BaseClass2()
+    {
+        System.Console.WriteLine("This is BaseClass2");
+    }
+}
+
+class DerivedClass : BaseClass1
+{
+    private BaseClass2 baseClass2;  // ใช้ Composition แทน
+
+    public DerivedClass()
+    {
+        baseClass2 = new BaseClass2();  // สร้าง instance ของ BaseClass2 แทนการสืบทอด
+        System.Console.WriteLine("This is DerivedClass");
+    }
+}
